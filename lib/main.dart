07afef450fd760f5da54ea8c8d066f412d339123
aarav123-android <69454819+aarav123-android@users.noreply.github.com>
2020-08-30 14:34:05 +0530

    Added Splash Screen and Logos

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,27 @@ import 'package:order_vegetables/models/user.dart';
 import 'package:order_vegetables/screens/wrapper.dart';
 import 'package:order_vegetables/services/auth.dart';
 import 'package:provider/provider.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
 
-void main() {
-  runApp(MyApp());
+void main(List<String> args) {
+  runApp(SplashScreen());
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AnimatedSplashScreen(
+        splash: Image.asset('assets/images/Logo.png'),
+        nextScreen: MyApp(),
+        splashTransition: SplashTransition.scaleTransition,
+        pageTransitionType: PageTransitionType.leftToRight,
+        duration: 3000,
+        backgroundColor: Colors.teal[100],
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
